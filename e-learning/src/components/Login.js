551dbc56243d673e6d4869f295c7637bf77a2ed9
saimurahari <laxmisaimurari2001@gmail.com>
2022-08-11 22:54:10 +0530

    login

--- e-learning/src/components/Login.js
+++ e-learning/src/components/Login.js
@@ -1,24 +1,36 @@
 import react from 'react';
 import './css/Login.css';
 import login from './Mobile-login-Cristina.jpg';
-
+import { useState } from 'react';
 function Login(){
-    
+    const[inputs, setInputs] = useState({});
+
+    const hanldeChange=(event)=>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values=>({...values,[name]:value}))
+
+    }
+    const handleSubmit=(event)=>{
+        event.preventDefault(); 
+        console.log(inputs);
+
+    }
     return(
         <>
         <div className="background">
             <header className="container">
                 <img src={login} width="55%" alt="login"/>
                 <div className="FormContainer">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                     <div className="form">
                         <h1>Login</h1>
                         <br></br>
                         <label>
-                            Email:<input type="email" name="username" placeholder="Enter Email"></input>
+                            Email:<input type="email" name="username" value={inputs.username||""} onChange={hanldeChange} placeholder="Enter Email"></input>
                         </label>
                         <label>
-                            Password:<input type="password" name="password" placeholder="Enter Password"></input>
+                            Password:<input type="password" name="password"  value={inputs.password||""} onChange={hanldeChange} placeholder="Enter Password"></input>
                         </label>
                         <br></br>
                         <center><input type="submit" value="Login"></input></center>
